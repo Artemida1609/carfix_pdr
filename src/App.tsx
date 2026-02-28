821be45e3bd9b32115cb1c67988542a0eba92841
Artem Skichko <artem.skichko@student.tuke.sk>
2026-02-28 10:19:35 +0100

    analytics/react

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { HomePage } from './pages/HomePage'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
